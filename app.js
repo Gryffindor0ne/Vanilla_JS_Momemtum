a00e27fe01bd641189702c68a31fc1ced6f353eb
Ryan Lee <ukjae29@gmail.com>
2021-10-04 11:59:17 +0900

    study_localStorage

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const onLoginSubmit = function (event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  greeting.innerText = `HELLO ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreedings(username);
 };
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreedings(username) {
+  // 반복 사용되는 코드 -> 함수화
+  greeting.innerText = `HELLO ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // LocalStorage 데이터 유뮤에 따른 폼 showing 구현
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreedings(savedUsername);
+}
