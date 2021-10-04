b200a51e11f81b371d08a931ec0d41e01c460dfe
Ryan Lee <ukjae29@gmail.com>
2021-10-04 11:18:32 +0900

    use hidden

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const loginForm = document.querySelector("#login-form");
-const loginButton = document.querySelector("#login-form button");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 const onLoginSubmit = function (event) {
   event.preventDefault();
-  console.log(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `HELLO ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 };
 
-function handleLinkClick(event) {
-  event.preventDefault(); // 기본 동작을 막는다. 오호!
-}
-
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
