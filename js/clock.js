ecb934dfdef7e80d123f078ee908f7da5ceaf5e6
Ryan Lee <ukjae29@gmail.com>
2021-10-05 14:20:21 +0900

    add : clock 1005

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0"); // padStart(원하는 자리수,앞쪽에 넣을 문자)
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+getClock();
+setInterval(getClock, 1000); // 정해진 시간마다 설정한 함수 실행
