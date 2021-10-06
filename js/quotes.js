35ae4179b42fb312d8164a0b87beb45bd128a301
Ryan Lee <ukjae29@gmail.com>
2021-10-06 14:36:27 +0900

    make a toDoList_1006

--- js/quotes.js
+++ js/quotes.js
@@ -1,54 +1,90 @@
 const quotes = [
   {
-    quote: "지금 있는 곳에서, 자기가 가진 것으로, 할 수 있는 일을 하라.",
-    author: "시어도어 루즈벨트",
+    quote: "You always pass failure on the way to success.",
+    transQuote: "성공하기까지 항상 실패를 거친다.",
+    author: "미키 루니",
+  },
+  {
+    quote: "A minute's success pays the failure of years.",
+    transQuote: "단 1분의 성공이 몇 년의 실패를 보상한다.",
+    author: "로버트 브라우닝",
+  },
+  {
+    quote: "Do not turn back when you are just at the goal.",
+    transQuote: "목표에 도달했을 때 돌아서지 마라.",
+    author: "퍼블릴리머스 사이러스",
+  },
+  {
+    quote: "Underpromise, overdeliver.",
+    transQuote: "덜 약속하고 더 해주어라.",
+    author: "톰 피터스",
+  },
+  {
+    quote: "There is no shortcut.",
+    transQuote: "지름길은 없다.",
+    author: "존 그루버",
+  },
+  {
+    quote: "Weakness of attitude becomes weakness of character.",
+    transQuote: "나약한 태도는 성격도 나약하게 만든다.",
+    author: "앨버트 아인슈타인",
+  },
+  {
+    quote: "Luck is what you have left over after you give 100 percent.",
+    transQuote: "행운은 100% 노력한 뒤에 남는 것이다.",
+    author: "랭스턴 콜만",
+  },
+  {
+    quote: "We are all growing.",
+    transQuote: "우리는 모두 성장하고 있다.",
+    author: "존 그루버",
+  },
+  {
+    quote: "Success is never final.",
+    transQuote: "성공이 끝은 아니다.",
+    author: "윈스턴 처칠",
   },
   {
     quote:
-      "열렬히 박수 받는 투자 활동에 주의하라. 위대한 조치는 대부분 조용하고 지루하고 재미가 없다.",
-    author: "웨렌 버핏",
+      "The world is full of suffering but it is also full of people overcoming it.",
+    transQuote:
+      "세상은 고통으로 가득하지만, 그것을 극복하는 사람들로도 가득하다.",
+    author: "헬렌 켈러",
+  },
+  {
+    quote: "A goal without a plan is just a wish.",
+    transQuote: "계획 없는 목표는 단지 꿈에 불과하다.",
+    author: "생텍쥐페리",
+  },
+  {
+    quote: "Everything comes to him who hustles while he waits.",
+    transQuote: "성공은 열심히 노력하며 기다리는 사람에게 온다.",
+    author: "토마스 에디슨",
+  },
+  {
+    quote: "Joy is not in things, it is in us.",
+    transQuote: "기쁨은 사물 안에 있지 않다. 그것은 우리 안에 있다.",
+    author: "리하르트 바그너",
+  },
+  {
+    quote: "Life is not fair: get used to it.",
+    transQuote: "인생은 공평하지 않다는 사실에 익숙해져라.",
+    author: "빌 게이츠",
   },
   {
     quote:
-      "사람들이 당신에 대해 뭐라고 쓰든지 상관하지 마라. 얼마나 길게 썼는지만 봐라.",
-    author: "앤디 워홀",
-  },
-  {
-    quote: "지식은 실천의 시작이다. 행동은 지식의 완성이다.",
-    author: "왕양명",
-  },
-  {
-    quote: "모든 일에는 언제나 위험보다 두려움이 더 많다.",
-    author: "세네카",
-  },
-  {
-    quote: "사람들은 달에 갈 생각만 하느라 자기 발밑에 핀 꽃을 보지 못한다.",
-    author: "알베르트 슈바이처",
-  },
-  {
-    quote: "개를 고용하지 말고 그냥 직접 짖어라.",
-    author: "데이비드 오길비",
-  },
-  {
-    quote:
-      "사람들이 아이디어를 훔칠까 봐 걱정할 필요 없다. 정말 독창적인 아이디어라면 목구멍에 쑤셔 넣어줘도 받아먹지 못할 테니까.",
-    author: "하워드 H. 에이컨",
-  },
-  {
-    quote:
-      "누구에게 공이 돌아갈지 신경 쓰지 않을 때 얼마나 많은 일을 이룰 수 있는지 보면 놀랍다.",
-    author: "해리 트루먼",
-  },
-  {
-    quote: "세상에서 보고 싶은 변화는 스스로 이루어야 한다.",
-    author: "마하트라 간디",
+      "They must often change who would be constant in happiness or wisdom.",
+    transQuote: "늘 행복하고 지혜로운 사람이 되려면 자주 변화해야 한다.",
+    author: "공자",
   },
 ];
 
 const quote = document.querySelector("#quote span:first-child");
+const transQuote = document.querySelector("#quote span:nth-child(2)");
 const author = document.querySelector("#quote span:last-child");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+transQuote.innerText = todaysQuote.transQuote;
+author.innerText = `- ${todaysQuote.author}`;
