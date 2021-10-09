cd088bc621578eb5f11c56afb30d8c97649c796d
Ryan Lee <ukjae29@gmail.com>
2021-10-09 12:39:49 +0900

    CSS wip

--- js/todo.js
+++ js/todo.js
@@ -26,7 +26,8 @@ function paintToDo(newToDo) {
   const span = document.createElement("span");
   span.innerText = `${newToDo.text}  `;
   const button = document.createElement("button");
-  button.innerText = "❌";
+  button.className = "todoBtn";
+  button.innerText = " ❌ ";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
